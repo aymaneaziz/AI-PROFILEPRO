96c796aa5ecbf102475dea0755e0557f04bcbaf0
aymenaziz <aymenaziz1234@gmail.com>
2025-06-09 17:00:43 +0100

    Correction des Images

--- client/src/portfolio/portfolio-spec/components/portfolio-form.jsx
+++ client/src/portfolio/portfolio-spec/components/portfolio-form.jsx
@@ -1500,7 +1500,7 @@ const handleProfilePictureUrl = (e) => {
                         <Input
                           type="file"
                           accept="image/*"
-                          onChange={handleProjectImageChang}
+                          onChange={handleProjectImageChange}
                           className="block w-full text-sm text-gray-500 file:mr-4 f file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700 hover:file:bg-indigo-100 cursor-pointer"
                         />
                       </div>
