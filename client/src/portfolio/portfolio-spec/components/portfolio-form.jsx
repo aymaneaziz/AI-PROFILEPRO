acc1559f28d75637d664a7b5710e5a3a4f1f2c72
aymenaziz <aymenaziz1234@gmail.com>
2025-06-11 20:05:59 +0100

    Correction URL

--- client/src/portfolio/portfolio-spec/components/portfolio-form.jsx
+++ client/src/portfolio/portfolio-spec/components/portfolio-form.jsx
@@ -169,7 +169,7 @@ export default function PortfolioForm({ onSubmit, initialData }) {
       try {
         setIsLoading(true);
         const response = await fetch(
-          "https://pfestageai-profilepro-production.up.railway.app/UploadImage/upload-resume.php",
+          "https://ai-profilepro-production.up.railway.app/UploadImage/upload-resume.php",
           {
             method: "POST",
             body: formData,
@@ -403,7 +403,7 @@ export default function PortfolioForm({ onSubmit, initialData }) {
       try {
         setIsLoading(true);
         const response = await fetch(
-          "https://pfestageai-profilepro-production.up.railway.app/UploadImage/upload-portfolio.php",
+          "https://ai-profilepro-production.up.railway.app/UploadImage/upload-portfolio.php",
           {
             method: "POST",
             body: formData,
